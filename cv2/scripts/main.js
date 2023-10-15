9933cd5fc7347e365806c5af8e01156a2a85970e
MyosQ <frejsundqvist@protonmail.com>
2023-10-15 19:15:37 +0200

    wip

--- cv2/scripts/main.js
+++ cv2/scripts/main.js
@@ -1,4 +1,28 @@
+function intersectionObservers() {
+    const sections = document.querySelectorAll('.slide-left, .scale-y');
+    const options = {
+        root: null,
+        threshold: 1.0,
+        rootMargin: '0px'
+    };
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('animate');
+            } else {
+                entry.target.classList.remove('animate');
+            }
+        });
+    }, options);
+    sections.forEach(section => {
+        observer.observe(section);
+    });
+}
+
+
+
 $(document).ready(() => {
     AOS.init();
+    intersectionObservers();
 });
     
