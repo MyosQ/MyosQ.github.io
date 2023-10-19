43b37532de3ba06a6d65dc8ce6e3fb9819405f25
MyosQ <frejsundqvist@protonmail.com>
2023-10-19 18:20:28 +0200

    wip

--- cv2/scripts/main.js
+++ cv2/scripts/main.js
@@ -24,5 +24,37 @@ function intersectionObservers() {
 $(document).ready(() => {
     AOS.init();
     intersectionObservers();
+
+
+
+
+
+    const tab_buttons = document.querySelectorAll('.tab-button');
+const skill_classes = document.querySelectorAll('.skill-class');
+const skills_container = document.getElementById('skills');
+
+tab_buttons.forEach((button, index) => {
+  button.addEventListener('click', () => {
+    // Remove active class from all
+    skill_classes.forEach(skill_class => {
+      skill_class.classList.remove('active');
+    });
+
+    // Add active class to the selected skill_class
+    const target = document.getElementById(`content${index + 1}`);
+    target.classList.add('active');
+
+    // Calculate the amount to scroll horizontally
+    const scroll_position = target.getBoundingClientRect().left - skills_container.getBoundingClientRect().left;
+    console.log(scroll_position);
+
+    // Use CSS scroll-behavior for smooth scrolling
+    skills_container.scrollTo({
+      right: scroll_position,
+      behavior: 'smooth'
+    });
+  });
+});
+
 });
     
