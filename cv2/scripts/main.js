5d7e0dac8e5658d9c8b6909d11b1efd39bf03093
MyosQ <frejsundqvist@protonmail.com>
2023-10-21 14:36:20 +0200

    wip

--- cv2/scripts/main.js
+++ cv2/scripts/main.js
@@ -2,7 +2,7 @@ function intersectionObservers() {
     const sections = document.querySelectorAll('.slide-left, .scale-y, .fade-in');
     const options = {
         root: null,
-        threshold: 1.0,
+        threshold: 0.1,
         rootMargin: '0px'
     };
     const observer = new IntersectionObserver((entries) => {
@@ -19,42 +19,45 @@ function intersectionObservers() {
     });
 }
 
+function initSkillClassStuff() {
+  const tab_buttons = document.querySelectorAll('.tab-button');
+  const skill_classes = document.querySelectorAll('.skill-class');
+  const skills_container = document.getElementById('skill-classes-all');
+
+  tab_buttons.forEach(tab => {
+    tab.addEventListener('click', () => {
+      skill_classes.forEach(skill_class => {
+        skill_class.classList.remove('active');
+      });
+      tab_buttons.forEach(tab_button => {
+        tab_button.classList.remove('active');
+      });
+
+      const skill_class_id = tab.getAttribute("data-skill-class-id");
+
+      const target = document.getElementById(skill_class_id);
+      target.classList.add('active');
+      tab.classList.add('active');
+
+      const scroll_position = target.getBoundingClientRect().left - skills_container.getBoundingClientRect().left;
+      skills_container.style.left = `-${scroll_position}px`;
+    });
+  });
+
+  const parent_element = document.querySelector('#skill-class-wrapper');
+  const child_element = document.querySelector('#skill-classes-all');
+  const child_height = Math.ceil(child_element.getBoundingClientRect().height);
+  parent_element.style.height = `${child_height}px`;
+}
 
 
 $(document).ready(() => {
     AOS.init();
     intersectionObservers();
-
-
-
-
-
-    const tab_buttons = document.querySelectorAll('.tab-button');
-const skill_classes = document.querySelectorAll('.skill-class');
-const skills_container = document.getElementById('skills');
-
-tab_buttons.forEach((button, index) => {
-  button.addEventListener('click', () => {
-    // Remove active class from all
-    skill_classes.forEach(skill_class => {
-      skill_class.classList.remove('active');
-    });
-
-    // Add active class to the selected skill_class
-    const target = document.getElementById(`content${index + 1}`);
-    target.classList.add('active');
-
-    // Calculate the amount to scroll horizontally
-    const scroll_position = target.getBoundingClientRect().left - skills_container.getBoundingClientRect().left;
-    console.log(scroll_position);
-
-    // Use CSS scroll-behavior for smooth scrolling
-    skills_container.scrollTo({
-      right: scroll_position,
-      behavior: 'smooth'
-    });
-  });
+    initSkillClassStuff(); 
 });
 
+$(window).resize(() => {
+    initSkillClassStuff();
 });
     
